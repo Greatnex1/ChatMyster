2496e755a4dd1c87bec630f137be23a07829c7f5
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-18 03:05:56 +0100

    add myster user response

---
+++ src/main/java/com/social/chat_myster/dto/MysterUserDto.java
@@ -0,0 +1,21 @@
+package com.social.chat_myster.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.social.chat_myster.data.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record MysterUserDto(
+         String id,
+        String firstname,
+        String lastname,
+        String email,
+        String phoneNumber,
+        String role
+) {
+}
