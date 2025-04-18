cd71037a8fb8a81ee3af230d73b7ebe3a4a8d198
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-18 21:55:38 +0100

    add user details implementation

---
+++ src/main/java/com/social/chat_myster/security/AuthenticatedUser.java
@@ -0,0 +1,59 @@
+package com.social.chat_myster.security;
+
+import com.social.chat_myster.data.entity.MysterUser;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+@AllArgsConstructor
+@Slf4j
+public class AuthenticatedUser implements UserDetails {
+
+    private final MysterUser user;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    public String getUserId(){
+        return user.getId();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
