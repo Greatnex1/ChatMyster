63ec0d8d74d117366468e93ed00c58cb179f9b71
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-18 03:05:40 +0100

    add myster user entity

---
+++ src/main/java/com/social/chat_myster/data/entity/MysterUser.java
@@ -0,0 +1,35 @@
+package com.social.chat_myster.data.entity;
+
+import com.social.chat_myster.data.enums.Role;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Builder
+public class MysterUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String firstname;
+    private String lastname;
+   @Column(unique= true)
+    @Email
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String confirmPassword;
+    private String username;
+    private String myUsername;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private LocalDateTime dateCreated;
+
+}
