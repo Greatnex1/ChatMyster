61d7026a98fe74b79e2bf776b3916fb8db539d95
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-18 21:50:28 +0100

    add raw query

--- src/main/java/com/social/chat_myster/repository/MysterUserRepository.java
+++ src/main/java/com/social/chat_myster/repository/MysterUserRepository.java
@@ -2,9 +2,13 @@ package com.social.chat_myster.repository;
 
 import com.social.chat_myster.data.entity.MysterUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MysterUserRepository extends JpaRepository<MysterUser, String> {
-    Optional<MysterUser> findByUsername(String username);
+    @Query("SELECT u FROM MysterUser u WHERE LOWER(u.username) = LOWER(:username)")
+    Optional<MysterUser> findByUsername(@Param("username") String username);
+
     MysterUser findUserByUsernameIgnoreCase(String username);}
