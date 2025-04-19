8b47bc17a7abd0e943aa23c4213f8a341707fd39
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-19 05:36:06 +0100

    add myster user service

---
+++ src/main/java/com/social/chat_myster/service/implementation/MysterUserService.java
@@ -0,0 +1,93 @@
+package com.social.chat_myster.service.implementation;
+
+import com.social.chat_myster.data.entity.MysterUser;
+import com.social.chat_myster.data.enums.Role;
+import com.social.chat_myster.dto.MysterUserDto;
+import com.social.chat_myster.dto.SignUpDto;
+import com.social.chat_myster.exception.MysterUserException;
+import com.social.chat_myster.repository.MysterUserRepository;
+import com.social.chat_myster.service.interfaces.MysterUserUseCase;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class MysterUserService implements MysterUserUseCase {
+
+    private final MysterUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public MysterUserDto createMysterUser(SignUpDto registerUser) throws MysterUserException {
+
+         ensureUsernameIsUniqueAndPasswordMatches(registerUser);
+
+        registerUser.validateSignUpData();
+
+        MysterUser newUser = MysterUser.builder()
+                .firstname(registerUser.firstname())
+                .lastname(registerUser.lastname())
+                .email(registerUser.email())
+                .username(registerUser.username())
+                .password(passwordEncoder.encode(registerUser.password()))
+                .phoneNumber(registerUser.phoneNumber())
+                .role(Role.valueOf(registerUser.role()))
+                .dateCreated(ZonedDateTime.now())
+                .build();
+     MysterUser saved =   userRepository.save(newUser);
+        log.info("A Myster User with username ->{}, and email->{}, registered successfully", saved.getUsername(), saved.getEmail());
+        return MysterUserDto.builder()
+                .id(newUser.getId())
+                .email(newUser.getEmail())
+                .firstname(newUser.getFirstname())
+                .lastname(newUser.getLastname())
+                .phoneNumber(newUser.getPhoneNumber())
+                .username(newUser.getUsername())
+                .role(String.valueOf(newUser.getRole()))
+                .dateCreated(ZonedDateTime.now())
+                .build();
+    }
+
+    private void ensureUsernameIsUniqueAndPasswordMatches(SignUpDto registerUser) throws MysterUserException {
+        Optional<MysterUser> existingUser = userRepository.findByUsername(registerUser.username());
+
+        if (existingUser.isPresent()) {
+            throw new MysterUserException("A myster user with this username already exists!", HttpStatus.CONFLICT);
+        }
+        if (!registerUser.password().equals(registerUser.confirmPassword())) {
+            throw new IllegalArgumentException("Password does not match");
+        }
+    }
+
+    @Override
+    public MysterUser findUserByUsername(String username) throws MysterUserException {
+        MysterUser existingUser = userRepository.findByUsername(username).orElseThrow(()->
+                new MysterUserException("User with this username not found", HttpStatus.NOT_FOUND));
+        return existingUser;
+    }
+
+    @Override
+    public MysterUser loadUser(String username) {
+        return userRepository.findUserByUsernameIgnoreCase(username);
+    }
+
+    @Override
+    public List<MysterUserDto> findAllUsers() {
+        return List.of();
+    }
+
+
+    @Override
+    public void verifyUser(String token) throws MysterUserException {
+
+    }
+
+}
