07f033c2c3bf25ff0af48a933d06a2c7bc5291c8
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-21 23:19:50 +0100

    add chat use case

---
+++ src/main/java/com/social/chat_myster/service/interfaces/ChatUseCase.java
@@ -0,0 +1,18 @@
+package com.social.chat_myster.service.interfaces;
+
+import com.social.chat_myster.data.entity.ChatRoom;
+import com.social.chat_myster.data.entity.ChatRoomMessage;
+import com.social.chat_myster.data.entity.MysterUser;
+import com.social.chat_myster.dto.response.ChatRoomResponse;
+
+import java.util.List;
+import java.util.Set;
+
+public interface ChatUseCase {
+    ChatRoom createGroupChat(MysterUser creator, String chatName, Set<String> participantIds);
+    void addParticipants(String chatRoomId, Set<String> participantIds);
+    void removeParticipant(String chatRoomId, String participantId);
+    ChatRoom getChatRoom(String roomId);
+    List<ChatRoomResponse> getUserChatRooms(MysterUser user);
+    void addMessage(String chatRoomId, ChatRoomMessage message);
+}
