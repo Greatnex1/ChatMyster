a9d46982c65d0b7cd1d9cc2122cb487bf36b6b4f
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-24 17:37:25 +0100

    refactor myster user use case

--- src/main/java/com/social/chat_myster/service/interfaces/MysterUserUseCase.java
+++ src/main/java/com/social/chat_myster/service/interfaces/MysterUserUseCase.java
@@ -3,15 +3,19 @@ package com.social.chat_myster.service.interfaces;
 import com.social.chat_myster.data.entity.MysterUser;
 import com.social.chat_myster.dto.MysterUserDto;
 import com.social.chat_myster.dto.SignUpDto;
+import com.social.chat_myster.dto.response.UserDetailResponse;
 import com.social.chat_myster.exception.MysterUserException;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface MysterUserUseCase {
 MysterUserDto createMysterUser(SignUpDto registerUser) throws MysterUserException;
     MysterUser loadUser(String username);
+    List<UserDetailResponse> getUsersDetails(Collection<String> userIds);
+
     MysterUser findUserByUsername (String userId) throws MysterUserException;
     List<MysterUser> findAllUsers();
 //    void updateUserProfile(String id, UpdateRequest updateRequest) throws MysterUserException;
-    void verifyUser(String token) throws MysterUserException;
+//    void verifyUser(String token) throws MysterUserException;
 }
