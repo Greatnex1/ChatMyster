f721561c703945e063a70617ba2c4a4ca2bd67ed
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-25 03:19:26 +0100

    refactor chat use_case

--- src/main/java/com/social/chat_myster/service/interfaces/ChatUseCase.java
+++ src/main/java/com/social/chat_myster/service/interfaces/ChatUseCase.java
@@ -4,13 +4,14 @@ import com.social.chat_myster.data.entity.ChatRoom;
 import com.social.chat_myster.data.entity.ChatRoomMessage;
 import com.social.chat_myster.data.entity.MysterUser;
 import com.social.chat_myster.dto.response.ChatRoomResponse;
+import com.social.chat_myster.exception.MysterUserException;
 
 import java.util.List;
 import java.util.Set;
 
 public interface ChatUseCase {
-    ChatRoom createGroupChat(MysterUser creator, String chatName, Set<String> participantIds);
-    void addParticipants(String chatRoomId, Set<String> participantIds);
+    ChatRoom createGroupChat(MysterUser creator, String chatName, Set<String> participantIds) throws MysterUserException;
+    void addParticipants(String chatRoomId, Set<String> participantIds) throws MysterUserException;
     void removeParticipant(String chatRoomId, String participantId);
     ChatRoom getChatRoom(String roomId);
     List<ChatRoomResponse> getUserChatRooms(MysterUser user);
