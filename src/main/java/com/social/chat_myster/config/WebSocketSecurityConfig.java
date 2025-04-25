f9d827472e1d884134d5dc17f255585e06d8354e
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-25 03:17:55 +0100

    add web socket security config

---
+++ src/main/java/com/social/chat_myster/config/WebSocketSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.social.chat_myster.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+
+@Configuration
+@EnableWebSocketSecurity
+public class WebSocketSecurityConfig {
+    @Bean
+    public AuthorizationManager<Message<?>> messageAuthorizationManager() {
+        return new AuthenticatedAuthorizationManager<>();
+    }
+}
